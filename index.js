6ed4408ef98b38532e29c9b9b592e0bacb98de42
Suphasuta Prajamsri <suphprajam95@gmail.com>
2022-04-13 18:52:28 -0700

    select drink functionality

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
 console.log("hi mama");
+const searchContainer = document.querySelector(".searchContainer");
+const btnSelectDrink = document.querySelector(".btnSelectDrink");
+btnSelectDrink.addEventListener("click", getSelectedDrink);
+
+function getSelectedDrink() {
+  let selectedDrink = document.querySelector(".drinksList").value;
+  if (selectedDrink) {
+    fetch(
+      `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${selectedDrink}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data.drinks);
+        drinkName.innerText = data.drinks[0].strDrink;
+        document.querySelector("img").src = data.drinks[0].strDrinkThumb;
+        recipe.innerText = data.drinks[0].strInstructions;
+      });
+  }
+  console.log(selectedDrink);
+}
+
 const drinkName = document.querySelector(".drinkName");
 const recipe = document.querySelector(".recipe");
-const button = document.querySelector(".button");
-button.addEventListener("click", getDrink);
+const btnRandom = document.querySelector(".btnRandom");
+btnRandom.addEventListener("click", getRandomDrink);
 
-function getDrink() {
+function getRandomDrink() {
   fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
     .then((res) => res.json())
     .then((data) => {
@@ -13,6 +34,7 @@ function getDrink() {
       document.querySelector("img").src = data.drinks[0].strDrinkThumb;
       recipe.innerText = data.drinks[0].strInstructions;
     });
+  searchContainer.style.display = "none";
 }
 
 // let i = 0;
