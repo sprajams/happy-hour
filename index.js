9ad1c95752a52e8fe7a0621b95e322eb24fd0513
Suphasuta Prajamsri <suphprajam95@gmail.com>
2022-04-14 15:11:44 -0700

    add in ingredients and measurements

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-console.log("hi mama");
 const searchContainer = document.querySelector(".searchContainer");
 const btnSelectDrink = document.querySelector(".btnSelectDrink");
 btnSelectDrink.addEventListener("click", getSelectedDrink);
@@ -11,46 +10,63 @@ function getSelectedDrink() {
     )
       .then((res) => res.json())
       .then((data) => {
-        console.log(data.drinks);
+        // console.log(data.drinks);
         drinkName.innerText = data.drinks[0].strDrink;
         document.querySelector("img").src = data.drinks[0].strDrinkThumb;
         recipe.innerText = data.drinks[0].strInstructions;
       });
+    searchContainer.style.display = "none";
   }
-  console.log(selectedDrink);
+  //   console.log(selectedDrink);
 }
 
 const drinkName = document.querySelector(".drinkName");
 const recipe = document.querySelector(".recipe");
 const btnRandom = document.querySelector(".btnRandom");
 btnRandom.addEventListener("click", getRandomDrink);
+const ingredients = document.querySelector(".ingredients");
+const drinkInfoContainer = document.querySelector(".drinkInfoContainer");
 
 function getRandomDrink() {
+  ingredients.innerHTML = "";
+  recipe.innerText = "";
   fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
     .then((res) => res.json())
     .then((data) => {
-      console.log(data.drinks);
-      drinkName.innerText = data.drinks[0].strDrink;
-      document.querySelector("img").src = data.drinks[0].strDrinkThumb;
-      recipe.innerText = data.drinks[0].strInstructions;
-    });
-  searchContainer.style.display = "none";
-}
+      //   console.log(data.drinks);
+      let drinkData = data.drinks[0];
+      drinkName.innerHTML = `Today's drink: <br/>` + drinkData.strDrink;
+      document.querySelector("img").src = drinkData.strDrinkThumb;
+      recipe.innerText += drinkData.strInstructions;
 
-// let i = 0;
-// function getDrink() {
-//     fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita")
-//       .then((res) => res.json())
-//       .then((data) => {
-//         console.log(data.drinks);
-//         console.log(data.drinks.length);
-//         if (i < data.drinks.length) {
-//           drinkName.innerText = data.drinks[i].strDrink;
-//           document.querySelector("img").src = data.drinks[i].strDrinkThumb;
-//           recipe.innerText = data.drinks[i].strInstructions;
-//           i++;
-//         } else {
-//           i = 0;
-//         }
-//       });
-//   }
+      let j = 1;
+      let ingredientElements = "";
+      while (drinkData["strIngredient" + j]) {
+        if (!drinkData["strMeasure" + j]) {
+          drinkData["strMeasure" + j] = "";
+        }
+        ingredientElements += `<div> ${drinkData["strMeasure" + j]} ${
+          drinkData["strIngredient" + j]
+        } </div>`;
+        j++;
+      }
+      ingredients.innerHTML += ingredientElements;
+
+      //   const node = document.createElement("div");
+      //   const innerNode = document.createElement("div");
+      //   while (drinkData["strIngredient" + j]) {
+      //     ingredients.innerHTML +=
+      //       drinkData["strMeasure" + j] +
+      //       ` ` +
+      //       drinkData["strIngredient" + j] +
+      //       `<br/>`;
+      //     j++;
+      //   }
+      //   node.append(ingredientElements);
+      //   //   node.append(innerNode);
+      //   ingredients.append(node);
+      //   innerNode.append(drinkData["strIngredient" + j]);
+
+      searchContainer.style.display = "none";
+    });
+}
