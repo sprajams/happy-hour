ceb57a74ea7df394213c8681b1730886f5deab25
Suphasuta Prajamsri <suphprajam95@gmail.com>
2022-04-15 15:00:41 -0700

    refactor code usable for random & select drinks

--- index.js
+++ index.js
@@ -1,46 +1,37 @@
 const searchContainer = document.querySelector(".searchContainer");
-const btnSelectDrink = document.querySelector(".btnSelectDrink");
-btnSelectDrink.addEventListener("click", getSelectedDrink);
-
-function getSelectedDrink() {
-  let selectedDrink = document.querySelector(".drinksList").value;
-  if (selectedDrink) {
-    fetch(
-      `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${selectedDrink}`
-    )
-      .then((res) => res.json())
-      .then((data) => {
-        // console.log(data.drinks);
-        drinkName.innerText = data.drinks[0].strDrink;
-        document.querySelector(".drinkImg").src = data.drinks[0].strDrinkThumb;
-        recipe.innerText = data.drinks[0].strInstructions;
-      });
-    searchContainer.style.display = "none";
-  }
-  //   console.log(selectedDrink);
-}
-
 const drinkName = document.querySelector(".drinkName");
 const recipe = document.querySelector(".recipe");
-const btnRandom = document.querySelector(".btnRandom");
-btnRandom.addEventListener("click", getRandomDrink);
 const ingredients = document.querySelector(".ingredients");
 const drinkInfoContainer = document.querySelector(".drinkInfoContainer");
 
-function getRandomDrink() {
+function getSelectedURL() {
+  let selectedDrink = document.querySelector(".drinksList").value;
+  let url = "";
+  if (selectedDrink) {
+    url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${selectedDrink}`;
+    return url;
+  }
+}
+
+function getDrink(url) {
   ingredients.innerHTML = "";
   recipe.innerText = "";
-  fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
+  fetch(url)
     .then((res) => res.json())
     .then((data) => {
-      console.log(data.drinks);
-      let drinkData = data.drinks[0];
-      console.log(drinkData.strDrinkThumb);
+      //if drink category has multiple drinks, grab a random one
+      let numDrinks = data.drinks.length;
+      function getRandomInt(max) {
+        return Math.floor(Math.random() * max);
+      }
+      let randomDrinkNum = getRandomInt(numDrinks);
+      let drinkData = data.drinks[randomDrinkNum];
 
       drinkName.innerHTML = `Today's drink: <br/>` + drinkData.strDrink;
       document.querySelector(".drinkImg").src = drinkData.strDrinkThumb;
       recipe.innerText += drinkData.strInstructions;
 
+      //
       let j = 1;
       let ingredientElements = "";
       while (drinkData["strIngredient" + j]) {
@@ -53,22 +44,34 @@ function getRandomDrink() {
         j++;
       }
       ingredients.innerHTML += ingredientElements;
-
-      //   const node = document.createElement("div");
-      //   const innerNode = document.createElement("div");
-      //   while (drinkData["strIngredient" + j]) {
-      //     ingredients.innerHTML +=
-      //       drinkData["strMeasure" + j] +
-      //       ` ` +
-      //       drinkData["strIngredient" + j] +
-      //       `<br/>`;
-      //     j++;
-      //   }
-      //   node.append(ingredientElements);
-      //   //   node.append(innerNode);
-      //   ingredients.append(node);
-      //   innerNode.append(drinkData["strIngredient" + j]);
       drinkInfoContainer.style.display = "flex";
       searchContainer.style.display = "none";
     });
 }
+
+const btnSelectDrink = document.querySelector(".btnSelectDrink");
+btnSelectDrink.addEventListener("click", getSelectedDrink);
+function getSelectedDrink() {
+  getDrink(getSelectedURL());
+}
+
+const btnRandom = document.querySelector(".btnRandom");
+btnRandom.addEventListener("click", getRandomDrink);
+function getRandomDrink() {
+  getDrink("https://www.thecocktaildb.com/api/json/v1/1/random.php");
+}
+
+//   const node = document.createElement("div");
+//   const innerNode = document.createElement("div");
+//   while (drinkData["strIngredient" + j]) {
+//     ingredients.innerHTML +=
+//       drinkData["strMeasure" + j] +
+//       ` ` +
+//       drinkData["strIngredient" + j] +
+//       `<br/>`;
+//     j++;
+//   }
+//   node.append(ingredientElements);
+//   //   node.append(innerNode);
+//   ingredients.append(node);
+//   innerNode.append(drinkData["strIngredient" + j]);
